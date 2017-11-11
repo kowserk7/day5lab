b0c4905608521b3c78d1635e60e1b07e93e83e60
Kowser Kassa <kowserk7@gmail.com>
2017-11-10 16:20:42 -0800

    the second function works

--- app.js
+++ app.js
@@ -12,10 +12,8 @@ function sum (a,b){ //eslint-disable-line
   return array;
 }
 
-sum(4,7);
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
-console.log(testSum);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -26,11 +24,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+  var multiply = a * b;
+  var array = [sum,'The sum of '+a+' and '+b+' is '+multiply];
+  return array;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+multiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
